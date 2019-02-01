f5f73489a5ab4d1ce45f8bd242bee946c0c7dfd7
JanHuangLong <janhuanglong@gmail.com>
2019-02-01 14:39:21 +0800

    Hihihi

--- app/src/androidTest/java/com/JanChang/flashchatnewfirebase/ApplicationTest.java
+++ app/src/androidTest/java/com/JanChang/flashchatnewfirebase/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.JanChang.flashchatnewfirebase;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/JanChang/flashchatnewfirebase/ExampleUnitTest.java
+++ app/src/test/java/com/JanChang/flashchatnewfirebase/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.JanChang.flashchatnewfirebase;
 
 import org.junit.Test;
 
